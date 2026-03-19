dff788e26bd349b23a14ca21aa07d20753f8ace5
Shadrack Mutua <shadrackmutune9@gmail.com>
2026-03-19 11:26:25 +0300

    Fix Render Apache .htaccess Header issue

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-/**
- * PlotConnect - Main API Router
- */
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once 'config.php';
 
 // Get request method and endpoint
